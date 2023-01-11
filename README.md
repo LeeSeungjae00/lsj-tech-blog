b15967b8b7282d778db74af24bfa2a5a80652700
LeeSeungjae00 <seungjae266789@gmail.com>
2023-01-11 11:05:57 +0900

    :sparkles: Intrudoce new features.
    eslint 추가

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+## Todo
+
+- buy me a coffe
+- Front matter 에 tags
+- 검색 기능
+- 반응형 디자인
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://nextjs-tutorial-2rseatng3-leeseungjae00.vercel.app',
