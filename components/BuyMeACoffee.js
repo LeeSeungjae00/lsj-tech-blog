3bc2314b151a3708ce6760e505236ff425d91d03
LeeSeungjae00 <seungjae266789@gmail.com>
2023-04-17 14:03:37 +0900

    :sparkles: Introduce new features.
    buy me a coffee 추가

---
+++ components/BuyMeACoffee.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BuyMeACoffee() {
+  return (
+    <a href="https://www.buymeacoffee.com/LeeSeungjae"><img style={{ margin: 'auto' }} src="https://img.buymeacoffee.com/button-api/?text=커피 사주세요&emoji=&slug=LeeSeungjae&button_colour=FFDD00&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=ffffff" /></a>
+  )
+}
