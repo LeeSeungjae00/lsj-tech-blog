6f6ca595b4d672c3c04305c67b645c25c0151d90
LeeSeungjae00 <seungjae266789@gmail.com>
2023-04-17 11:51:58 +0900

    :bug: Fix a bug.
    우터런스 댓글 기능 레포지토리 이름 변경으로 인해 안나오는것 수정

--- components/Utterances.js
+++ components/Utterances.js
@@ -9,7 +9,7 @@ function Utterances() {
       const scriptElement = document.createElement('script')
       scriptElement.src = "https://utteranc.es/client.js"
       scriptElement.async = true
-      scriptElement.setAttribute('repo', "LeeSeungjae00/nextjs-tutorial")
+      scriptElement.setAttribute('repo', "LeeSeungjae00/lsj-tech-blog")
       scriptElement.setAttribute('issue-term', "pathname")
       scriptElement.setAttribute('label', "✨💬✨")
       scriptElement.setAttribute('theme', "github-dark-orange")
